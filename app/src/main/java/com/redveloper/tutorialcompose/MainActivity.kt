f196fac3bc3ad680082cee23b10467ccf4518491
renaldy <renalraion@gmail.com>
2022-12-10 05:04:33 +0700

    add banner and searchbar

--- app/src/main/java/com/redveloper/tutorialcompose/MainActivity.kt
+++ app/src/main/java/com/redveloper/tutorialcompose/MainActivity.kt
@@ -3,14 +3,22 @@ package com.redveloper.tutorialcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.redveloper.tutorialcompose.ui.components.SearchBar
 import com.redveloper.tutorialcompose.ui.theme.TutorialComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,5 +48,22 @@ fun DefaultPreview() {
 
 @Composable
 fun JetpackCofeeApp(){
+    Column {
+        Banner()
+    }
+}
 
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier
+){
+    Box(modifier = modifier){
+        Image(
+            painter = painterResource(id = R.drawable.banner),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        SearchBar()
+    }
 }
---
+++ app/src/main/java/com/redveloper/tutorialcompose/ui/components/SearchBar.kt
@@ -0,0 +1,52 @@
+package com.redveloper.tutorialcompose.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.redveloper.tutorialcompose.ui.theme.TutorialComposeTheme
+import com.redveloper.tutorialcompose.R
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier
+){
+    TextField(
+        value = "", onValueChange = {},
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .heightIn(48.dp)
+            .clip(RoundedCornerShape(10.dp)),
+        leadingIcon = {
+            Icon(imageVector = Icons.Default.Search, contentDescription = null)
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface,
+            disabledIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        placeholder = {
+            Text(text = stringResource(id = R.string.placeholder_search))
+        },
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SearchBarPreview(){
+    TutorialComposeTheme {
+        SearchBar()
+    }
+}
