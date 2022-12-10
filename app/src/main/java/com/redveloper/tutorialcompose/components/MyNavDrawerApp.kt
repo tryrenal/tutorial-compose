92209ef93df0bea6265add45940caecb2ff0e66a
renaldy <renalraion@gmail.com>
2022-12-10 21:26:54 +0700

    learn about state holder

--- app/src/main/java/com/redveloper/tutorialcompose/components/MyNavDrawerApp.kt
+++ app/src/main/java/com/redveloper/tutorialcompose/components/MyNavDrawerApp.kt
@@ -1,66 +1,34 @@
 package com.redveloper.tutorialcompose.components
 
-import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
-import androidx.compose.material.SnackbarResult
 import androidx.compose.material.Text
-import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import com.redveloper.tutorialcompose.R
-import kotlinx.coroutines.launch
 
 @Composable
 fun MyNavDrawerApp(){
-    val scaffoldState = rememberScaffoldState()
-    val scope = rememberCoroutineScope()
-    val context = LocalContext.current
-
+    val appState = rememberMyNavDrawerState()
     Scaffold(
-        scaffoldState = scaffoldState,
+        scaffoldState = appState.scaffoldState,
         topBar = {
             MyTopBar(
-                onMenuClick = {
-                    scope.launch {
-                        scaffoldState.drawerState.open()
-                    }
-                }
+                onMenuClick = {appState.onMenuClick()}
             )
         },
         drawerContent = {
             MyDrawerContent(
                 onItemSelected = {
-                    scope.launch {
-                        scaffoldState.drawerState.close()
-                        val snackbar = scaffoldState.snackbarHostState.showSnackbar(
-                            message = context.resources.getString(R.string.coming_soon, it),
-                            actionLabel = context.resources.getString(R.string.subscribe_question)
-                        )
-                        if(snackbar == SnackbarResult.ActionPerformed){
-                            Toast.makeText(context, context.getString(R.string.subscribed_info), Toast.LENGTH_SHORT).show()
-                        }
-                    }
+                    appState.onItemSelected(it)
                 },
-                onBackPress = {
-                    if (scaffoldState.drawerState.isOpen){
-                        scope.launch {
-                            scaffoldState.drawerState.close()
-                        }
-                    } else {
-                        scope.launch {
-                            
-                        }
-                    }
-                }
+                onBackPress = { appState.onBackPress() }
             )
         },
-        drawerGesturesEnabled = scaffoldState.drawerState.isOpen
+        drawerGesturesEnabled = appState.scaffoldState.drawerState.isOpen
     ) { paddingValues ->
         Box(
             modifier = Modifier
