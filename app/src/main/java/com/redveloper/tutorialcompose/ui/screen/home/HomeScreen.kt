48a2be7375bfb4d7e4b9a92fa33e22108911f750
renaldy <renalraion@gmail.com>
2022-12-11 17:29:10 +0700

    show list reward in home

--- app/src/main/java/com/redveloper/tutorialcompose/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/redveloper/tutorialcompose/ui/screen/home/HomeScreen.kt
@@ -1,22 +1,55 @@
 package com.redveloper.tutorialcompose.ui.screen.home
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import com.redveloper.tutorialcompose.R
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.redveloper.tutorialcompose.di.Injection
+import com.redveloper.tutorialcompose.model.OrderReward
+import com.redveloper.tutorialcompose.ui.ViewModelFactory
+import com.redveloper.tutorialcompose.ui.common.UiState
+import com.redveloper.tutorialcompose.ui.components.RewardItem
 
 @Composable
 fun HomeScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = ViewModelFactory(Injection.provideRepository()))
 ) {
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
-    ) {
-        Text(stringResource(R.string.menu_home))
+    viewModel.uiState.collectAsState(initial = UiState.Loading).value.let { uistate ->
+        when(uistate){
+            is UiState.Loading -> {
+                viewModel.getAllRewards()
+            }
+            is UiState.Success -> {
+                HomeContent(orderReward = uistate.data, modifier = modifier)
+            }
+            is UiState.Error -> {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeContent(
+    orderReward: List<OrderReward>,
+    modifier: Modifier = Modifier
+){
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(160.dp),
+        contentPadding = PaddingValues(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier,
+    ){
+        items(orderReward){ item ->
+            RewardItem(image = item.reward.image, title = item.reward.title, requiredPoint = item.reward.requiredPoint)
+        }
     }
 }
