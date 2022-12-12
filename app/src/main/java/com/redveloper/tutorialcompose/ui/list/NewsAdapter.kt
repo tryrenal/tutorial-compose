0f6b931a215b0d3a0091fb44b8d43bf0ce8dccf0
renaldy <renalraion@gmail.com>
2022-12-13 00:07:40 +0700

    change item xml to compose

--- app/src/main/java/com/redveloper/tutorialcompose/ui/list/NewsAdapter.kt
+++ app/src/main/java/com/redveloper/tutorialcompose/ui/list/NewsAdapter.kt
@@ -3,12 +3,25 @@ package com.redveloper.tutorialcompose.ui.list
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
-import com.redveloper.tutorialcompose.R
+import coil.compose.AsyncImage
 import com.redveloper.tutorialcompose.data.local.entity.NewsEntity
 import com.redveloper.tutorialcompose.databinding.ItemNewsBinding
 
@@ -30,19 +43,83 @@ class NewsAdapter(private val onItemClick: (NewsEntity) -> Unit) : ListAdapter<N
         binding.root
     ) {
         fun bind(news: NewsEntity) {
-            binding.tvItemTitle.text = news.title
-            binding.tvItemPublishedDate.text = news.publishedAt
-            Glide.with(itemView.context)
-                .load(news.urlToImage)
-                .apply(
-                    RequestOptions.placeholderOf(R.drawable.ic_loading).error(R.drawable.ic_error)
-                )
-                .into(binding.imgPoster)
-            itemView.setOnClickListener {
-                onItemClick(news)
+            binding.composeView.setContent {
+                MaterialTheme{
+                    NewsItem(
+                        news.urlToImage,
+                        news.title,
+                        news.publishedAt,
+                        onItemClick = {
+                            onItemClick(news)
+                        }
+                    )
+                }
             }
         }
+
+        @Composable
+        fun NewsItem(
+            urlToImage: String?,
+            title: String,
+            publishedAt: String,
+            onItemClick: () -> Unit
+        ){
+            ConstraintLayout(
+                modifier = Modifier.clickable {
+                    onItemClick()
+                }
+            ) {
+                val (posterImage, titleText, publishedDateText) = createRefs()
+
+                AsyncImage(
+                    model = urlToImage,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(120.dp)
+                        .constrainAs(posterImage) {
+                            top.linkTo(parent.top)
+                        }
+                )
+                Text(
+                    text = title,
+                    overflow = TextOverflow.Ellipsis,
+                    textAlign = TextAlign.Start,
+                    maxLines = 3,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .constrainAs(titleText){
+                            start.linkTo(posterImage.end, margin = 16.dp)
+                            top.linkTo(parent.top, margin = 16.dp)
+                            end.linkTo(parent.end, margin = 16.dp)
+                            width = Dimension.fillToConstraints
+                        }
+                )
+                Text(
+                    text = publishedAt,
+                    fontSize = 14.sp,
+                    modifier = Modifier
+                        .constrainAs(publishedDateText){
+                            top.linkTo(titleText.bottom, margin = 8.dp)
+                            start.linkTo(titleText.start)
+                        }
+                )
+            }
+        }
+
+        @Preview(showBackground = true)
+        @Composable
+        fun NewsItemsPreview(){
+            MaterialTheme {
+                NewsItem(urlToImage = "", title = "New News", publishedAt = "2022-02-22") {
+
+                }
+            }
+        }
+
     }
+
 
     companion object {
         val DIFF_CALLBACK: DiffUtil.ItemCallback<NewsEntity> =
